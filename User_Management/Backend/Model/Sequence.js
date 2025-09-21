b98e098387d77f98624e30a77cf3b344fcec9a9d
Idusara <126001751+Idusara@users.noreply.github.com>
2025-09-21 12:45:18 +0530

    ok

--- User_Management/Backend/Model/Sequence.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const SequenceSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  value: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.models.Sequence || mongoose.model('Sequence', SequenceSchema);
