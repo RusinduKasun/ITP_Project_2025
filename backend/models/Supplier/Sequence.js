6f067d8836ef7652896f26710b821a33bdf4af9d
Rusindu-kasun <rusindukasundushshantha@gmail.com>
2025-09-28 22:53:29 +0530

    update

--- backend/models/Supplier/Sequence.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const SequenceSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  value: { type: Number, default: 0 }
-});
-
-export default mongoose.model('Sequence', SequenceSchema);
