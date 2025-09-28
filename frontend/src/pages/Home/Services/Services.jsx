8c7cf0bfee9bcdfff715d6d6fab4c7dd64aa2a05
Rusindu-kasun <rusindukasundushshantha@gmail.com>
2025-09-28 23:31:30 +0530

    (home): add homepage UI

---
+++ frontend/src/pages/Home/Services/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from "../Nav/Nav"
+function Services() {
+  return (
+    <div>
+
+        <Nav/>
+      <h1>Welcome to service page </h1>
+    </div>
+  )
+}
+
+export default Services;
+
+
+
